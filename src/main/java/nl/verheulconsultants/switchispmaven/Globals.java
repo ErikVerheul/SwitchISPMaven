5138ce8217c19f00b8e001f9146da6ee9c35f126
Erik Verheul <erik@verheulconsultants.nl>
2018-10-19 16:02:59 +0200

    Format source.

--- src/main/java/nl/verheulconsultants/switchispmaven/Globals.java
+++ src/main/java/nl/verheulconsultants/switchispmaven/Globals.java
@@ -9,393 +9,394 @@ import java.util.logging.Level;
  *
  * @author Erik
  */
- class Globals {
-    static final long ONE_SECOND = 1000L;
-    static final long FIVE_SECONDS = 5000L;
-    static final long DEFAULT_TRIGGERDURATION_IN_SEC = 30L;
-    static final long DEFAULT_RETRYINTERVAL_IN_SEC = 300L;
-    static final int DEFAULT_MAXRETRIES = 5;
-    static final int PRIMARY_ISP = 0;
-    static final int BACKUP_ISP = 1;
-    static final int HTTP_PORT = 80;
-    static final int TIME_OUT = 2000;
-    
-    private List<String> hosts;
-    private long lastContactWithAnyHost;
-    private String propsFileName;
-    private Properties props;
-    private boolean propertiesSetTemporarely;
-    private long successfulChecks;
-    private long failedChecks;
-    private int switchoverCount;
-    // these values must be set to real values using JConsole
-    private int currentISP;
-    private long triggerDuration;
-    private long retryInterval;
-    private int maxRetries;
-    private boolean backupISPselected;
-    private String emailAddress;
-    private String logFileName;
-    private String primaryISPscript;
-    private String backupISPscript;
-    private String primarySMTPserver;
-    private String backupSMTPserver;
-    private Level currentLogLevel;
-    // end of members which can be changed by the user
-    private boolean simulatePrimaryISPIsDown;
-    private boolean simulateBackupISPIsDown;
-    private boolean mockCheckISPisOK = false;
+class Globals {
 
-    Globals(String logFileName) {
-        //set the default logfile name from spring.xml
-        this.logFileName = logFileName;
-        hosts = new ArrayList<String>();
-        lastContactWithAnyHost = System.currentTimeMillis();
-        propsFileName = "SwitchISPservice.properties";
-        props = new Properties();
-        propertiesSetTemporarely = false;
-        successfulChecks = 0L;
-        failedChecks = 0L;
-        switchoverCount = 0;
-        simulatePrimaryISPIsDown = false;
-        simulateBackupISPIsDown = false;
+  static final long ONE_SECOND = 1000L;
+  static final long FIVE_SECONDS = 5000L;
+  static final long DEFAULT_TRIGGERDURATION_IN_SEC = 30L;
+  static final long DEFAULT_RETRYINTERVAL_IN_SEC = 300L;
+  static final int DEFAULT_MAXRETRIES = 5;
+  static final int PRIMARY_ISP = 0;
+  static final int BACKUP_ISP = 1;
+  static final int HTTP_PORT = 80;
+  static final int TIME_OUT = 2000;
 
-        // initialize with temporary values which must be set to real values using JConsole
-        currentISP = PRIMARY_ISP;
-        triggerDuration = DEFAULT_TRIGGERDURATION_IN_SEC;
-        retryInterval = DEFAULT_RETRYINTERVAL_IN_SEC;
-        maxRetries = DEFAULT_MAXRETRIES;
-        backupISPselected = false;
-        emailAddress = "me@mydomain.dom";
-        primaryISPscript = "c:\\tmp\\a.bat";
-        backupISPscript = "c:\\tmp\\b.bat";
-        primarySMTPserver = "smtp.primarySMTPserver.dom";
-        backupSMTPserver = "smtp.backupSMTPserver.dom";
-        currentLogLevel = Level.INFO;
-        // end of temporary initialization which can be changed by the user
+  private List<String> hosts;
+  private long lastContactWithAnyHost;
+  private String propsFileName;
+  private Properties props;
+  private boolean propertiesSetTemporarely;
+  private long successfulChecks;
+  private long failedChecks;
+  private int switchoverCount;
+  // these values must be set to real values using JConsole
+  private int currentISP;
+  private long triggerDuration;
+  private long retryInterval;
+  private int maxRetries;
+  private boolean backupISPselected;
+  private String emailAddress;
+  private String logFileName;
+  private String primaryISPscript;
+  private String backupISPscript;
+  private String primarySMTPserver;
+  private String backupSMTPserver;
+  private Level currentLogLevel;
+  // end of members which can be changed by the user
+  private boolean simulatePrimaryISPIsDown;
+  private boolean simulateBackupISPIsDown;
+  private boolean mockCheckISPisOK = false;
 
-    }
+  Globals(String logFileName) {
+    //set the default logfile name from spring.xml
+    this.logFileName = logFileName;
+    hosts = new ArrayList<String>();
+    lastContactWithAnyHost = System.currentTimeMillis();
+    propsFileName = "SwitchISPservice.properties";
+    props = new Properties();
+    propertiesSetTemporarely = false;
+    successfulChecks = 0L;
+    failedChecks = 0L;
+    switchoverCount = 0;
+    simulatePrimaryISPIsDown = false;
+    simulateBackupISPIsDown = false;
 
-    /**
-     * @return the triggerDuration
-     */
-     long getTriggerDuration() {
-        return triggerDuration;
-    }
+    // initialize with temporary values which must be set to real values using JConsole
+    currentISP = PRIMARY_ISP;
+    triggerDuration = DEFAULT_TRIGGERDURATION_IN_SEC;
+    retryInterval = DEFAULT_RETRYINTERVAL_IN_SEC;
+    maxRetries = DEFAULT_MAXRETRIES;
+    backupISPselected = false;
+    emailAddress = "me@mydomain.dom";
+    primaryISPscript = "c:\\tmp\\a.bat";
+    backupISPscript = "c:\\tmp\\b.bat";
+    primarySMTPserver = "smtp.primarySMTPserver.dom";
+    backupSMTPserver = "smtp.backupSMTPserver.dom";
+    currentLogLevel = Level.INFO;
+    // end of temporary initialization which can be changed by the user
 
-    /**
-     * @param triggerDuration the triggerDuration to set
-     */
-     void setTriggerDuration(long triggerDuration) {
-        this.triggerDuration = triggerDuration;
-    }
+  }
 
-    /**
-     * @return the currentLogLevel
-     */
-     Level getCurrentLogLevel() {
-        return currentLogLevel;
-    }
+  /**
+   * @return the triggerDuration
+   */
+  long getTriggerDuration() {
+    return triggerDuration;
+  }
 
-    /**
-     * @param currentLogLevel the currentLogLevel to set
-     */
-     void setCurrentLogLevel(Level currentLogLevel) {
-        this.currentLogLevel = currentLogLevel;
-    }
+  /**
+   * @param triggerDuration the triggerDuration to set
+   */
+  void setTriggerDuration(long triggerDuration) {
+    this.triggerDuration = triggerDuration;
+  }
 
-    /**
-     * @return the propertiesSetTemporarely
-     */
-     boolean isPropertiesSetTemporarely() {
-        return propertiesSetTemporarely;
-    }
+  /**
+   * @return the currentLogLevel
+   */
+  Level getCurrentLogLevel() {
+    return currentLogLevel;
+  }
 
-    /**
-     * @return the successfulChecks
-     */
-     long getSuccessfulChecks() {
-        return successfulChecks;
-    }
-    
-    /**
-     * Increase successful checks with one
-     */
-     void increaseSuccessfulChecks() {
-        successfulChecks++;
-    }
+  /**
+   * @param currentLogLevel the currentLogLevel to set
+   */
+  void setCurrentLogLevel(Level currentLogLevel) {
+    this.currentLogLevel = currentLogLevel;
+  }
 
-    /**
-     * @return the failedChecks
-     */
-     long getFailedChecks() {
-        return failedChecks;
-    }
-    
-    /**
-     * Increase failed checks with one
-     */
-     void increaseFailedChecks() {
-        failedChecks++;
-    }
+  /**
+   * @return the propertiesSetTemporarely
+   */
+  boolean isPropertiesSetTemporarely() {
+    return propertiesSetTemporarely;
+  }
 
-    /**
-     * @return the switchoverCount
-     */
-     int getSwitchoverCount() {
-        return switchoverCount;
-    }
-    
-    /**
-     * Increase the switch-over count with one
-     */
-     void increaseSwitchoverCount() {
-        switchoverCount++;
-    }
+  /**
+   * @return the successfulChecks
+   */
+  long getSuccessfulChecks() {
+    return successfulChecks;
+  }
 
-    /**
-     * @return the currentISP
-     */
-     int getCurrentISP() {
-        return currentISP;
-    }
+  /**
+   * Increase successful checks with one
+   */
+  void increaseSuccessfulChecks() {
+    successfulChecks++;
+  }
 
-    /**
-     * @param currentISP the currentISP to set
-     */
-     void setCurrentISP(int currentISP) {
-        this.currentISP = currentISP;
-    }
+  /**
+   * @return the failedChecks
+   */
+  long getFailedChecks() {
+    return failedChecks;
+  }
 
-    /**
-     * @return the retryInterval
-     */
-     long getRetryInterval() {
-        return retryInterval;
-    }
+  /**
+   * Increase failed checks with one
+   */
+  void increaseFailedChecks() {
+    failedChecks++;
+  }
 
-    /**
-     * @param retryInterval the retryInterval to set
-     */
-     void setRetryInterval(long retryInterval) {
-        this.retryInterval = retryInterval;
-    }
+  /**
+   * @return the switchoverCount
+   */
+  int getSwitchoverCount() {
+    return switchoverCount;
+  }
 
-    /**
-     * @return the maxRetries
-     */
-     int getMaxRetries() {
-        return maxRetries;
-    }
+  /**
+   * Increase the switch-over count with one
+   */
+  void increaseSwitchoverCount() {
+    switchoverCount++;
+  }
 
-    /**
-     * @param maxRetries the maxRetries to set
-     */
-     void setMaxRetries(int maxRetries) {
-        this.maxRetries = maxRetries;
-    }
+  /**
+   * @return the currentISP
+   */
+  int getCurrentISP() {
+    return currentISP;
+  }
 
-    /**
-     * @return the backupISPselected
-     */
-     boolean isBackupISPselected() {
-        return backupISPselected;
-    }
+  /**
+   * @param currentISP the currentISP to set
+   */
+  void setCurrentISP(int currentISP) {
+    this.currentISP = currentISP;
+  }
 
-    /**
-     * @param backupISPselected the backupISPselected to set
-     */
-     void setBackupISPselected(boolean backupISPselected) {
-        this.backupISPselected = backupISPselected;
-    }
+  /**
+   * @return the retryInterval
+   */
+  long getRetryInterval() {
+    return retryInterval;
+  }
 
-    /**
-     * @return the emailAddress
-     */
-     String getEmailAddress() {
-        return emailAddress;
-    }
+  /**
+   * @param retryInterval the retryInterval to set
+   */
+  void setRetryInterval(long retryInterval) {
+    this.retryInterval = retryInterval;
+  }
 
-    /**
-     * @param emailAddress the emailAddress to set
-     */
-     void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
+  /**
+   * @return the maxRetries
+   */
+  int getMaxRetries() {
+    return maxRetries;
+  }
 
-    /**
-     * @return the logFileName
-     */
-     String getLogFileName() {
-        return logFileName;
-    }
+  /**
+   * @param maxRetries the maxRetries to set
+   */
+  void setMaxRetries(int maxRetries) {
+    this.maxRetries = maxRetries;
+  }
 
-    /**
-     * @param logFileName the logFileName to set
-     */
-     void setLogFileName(String logFileName) {
-        this.logFileName = logFileName;
-    }
+  /**
+   * @return the backupISPselected
+   */
+  boolean isBackupISPselected() {
+    return backupISPselected;
+  }
 
-    /**
-     * @return the primaryISPscript
-     */
-     String getPrimaryISPscript() {
-        return primaryISPscript;
-    }
+  /**
+   * @param backupISPselected the backupISPselected to set
+   */
+  void setBackupISPselected(boolean backupISPselected) {
+    this.backupISPselected = backupISPselected;
+  }
 
-    /**
-     * @param primaryISPscript the primaryISPscript to set
-     */
-     void setPrimaryISPscript(String primaryISPscript) {
-        this.primaryISPscript = primaryISPscript;
-    }
+  /**
+   * @return the emailAddress
+   */
+  String getEmailAddress() {
+    return emailAddress;
+  }
 
-    /**
-     * @return the backupISPscript
-     */
-     String getBackupISPscript() {
-        return backupISPscript;
-    }
+  /**
+   * @param emailAddress the emailAddress to set
+   */
+  void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
 
-    /**
-     * @param backupISPscript the backupISPscript to set
-     */
-     void setBackupISPscript(String backupISPscript) {
-        this.backupISPscript = backupISPscript;
-    }
+  /**
+   * @return the logFileName
+   */
+  String getLogFileName() {
+    return logFileName;
+  }
 
-    /**
-     * @return the primarySMTPserver
-     */
-     String getPrimarySMTPserver() {
-        return primarySMTPserver;
-    }
+  /**
+   * @param logFileName the logFileName to set
+   */
+  void setLogFileName(String logFileName) {
+    this.logFileName = logFileName;
+  }
 
-    /**
-     * @param primarySMTPserver the primarySMTPserver to set
-     */
-     void setPrimarySMTPserver(String primarySMTPserver) {
-        this.primarySMTPserver = primarySMTPserver;
-    }
+  /**
+   * @return the primaryISPscript
+   */
+  String getPrimaryISPscript() {
+    return primaryISPscript;
+  }
 
-    /**
-     * @return the backupSMTPserver
-     */
-     String getBackupSMTPserver() {
-        return backupSMTPserver;
-    }
+  /**
+   * @param primaryISPscript the primaryISPscript to set
+   */
+  void setPrimaryISPscript(String primaryISPscript) {
+    this.primaryISPscript = primaryISPscript;
+  }
 
-    /**
-     * @param backupSMTPserver the backupSMTPserver to set
-     */
-     void setBackupSMTPserver(String backupSMTPserver) {
-        this.backupSMTPserver = backupSMTPserver;
-    }
+  /**
+   * @return the backupISPscript
+   */
+  String getBackupISPscript() {
+    return backupISPscript;
+  }
 
-    /**
-     * @return the simulatePrimaryISPIsDown
-     */
-     boolean isSimulatePrimaryISPIsDown() {
-        return simulatePrimaryISPIsDown;
-    }
+  /**
+   * @param backupISPscript the backupISPscript to set
+   */
+  void setBackupISPscript(String backupISPscript) {
+    this.backupISPscript = backupISPscript;
+  }
 
-    /**
-     * @return the simulateBackupISPIsDown
-     */
-     boolean isSimulateBackupISPIsDown() {
-        return simulateBackupISPIsDown;
-    }
+  /**
+   * @return the primarySMTPserver
+   */
+  String getPrimarySMTPserver() {
+    return primarySMTPserver;
+  }
 
-    /**
-     * @return the mockCheckISPisOK
-     */
-     boolean isMockCheckISPisOK() {
-        return mockCheckISPisOK;
-    }
+  /**
+   * @param primarySMTPserver the primarySMTPserver to set
+   */
+  void setPrimarySMTPserver(String primarySMTPserver) {
+    this.primarySMTPserver = primarySMTPserver;
+  }
 
-    /**
-     * @return the lastContactWithAnyHost
-     */
-     long getLastContactWithAnyHost() {
-        return lastContactWithAnyHost;
-    }
+  /**
+   * @return the backupSMTPserver
+   */
+  String getBackupSMTPserver() {
+    return backupSMTPserver;
+  }
 
-    /**
-     * @param lastContactWithAnyHost the lastContactWithAnyHost to set
-     */
-     void setLastContactWithAnyHost(long lastContactWithAnyHost) {
-        this.lastContactWithAnyHost = lastContactWithAnyHost;
-    }
+  /**
+   * @param backupSMTPserver the backupSMTPserver to set
+   */
+  void setBackupSMTPserver(String backupSMTPserver) {
+    this.backupSMTPserver = backupSMTPserver;
+  }
 
-    /**
-     * @return the propsFileName
-     */
-     String getPropsFileName() {
-        return propsFileName;
-    }
+  /**
+   * @return the simulatePrimaryISPIsDown
+   */
+  boolean isSimulatePrimaryISPIsDown() {
+    return simulatePrimaryISPIsDown;
+  }
 
-    /**
-     * @param propsFileName the propsFileName to set
-     */
-     void setPropsFileName(String propsFileName) {
-        this.propsFileName = propsFileName;
-    }
+  /**
+   * @return the simulateBackupISPIsDown
+   */
+  boolean isSimulateBackupISPIsDown() {
+    return simulateBackupISPIsDown;
+  }
 
-    /**
-     * @return the props
-     */
-     Properties getProps() {
-        return props;
-    }
+  /**
+   * @return the mockCheckISPisOK
+   */
+  boolean isMockCheckISPisOK() {
+    return mockCheckISPisOK;
+  }
 
-    /**
-     * @param props the props to set
-     */
-     void setProps(Properties props) {
-        this.props = props;
-    }
+  /**
+   * @return the lastContactWithAnyHost
+   */
+  long getLastContactWithAnyHost() {
+    return lastContactWithAnyHost;
+  }
 
-    /**
-     * @param propertiesSetTemporarely the propertiesSetTemporarely to set
-     */
-     void setPropertiesSetTemporarely(boolean propertiesSetTemporarely) {
-        this.propertiesSetTemporarely = propertiesSetTemporarely;
-    }
+  /**
+   * @param lastContactWithAnyHost the lastContactWithAnyHost to set
+   */
+  void setLastContactWithAnyHost(long lastContactWithAnyHost) {
+    this.lastContactWithAnyHost = lastContactWithAnyHost;
+  }
 
-    /**
-     * @return the hosts
-     */
-     List<String> getHosts() {
-        return hosts;
-    }
+  /**
+   * @return the propsFileName
+   */
+  String getPropsFileName() {
+    return propsFileName;
+  }
 
-    /**
-     * @param hosts the hosts to set
-     */
-     void setHosts(List<String> hosts) {
-        this.hosts = hosts;
-    }
+  /**
+   * @param propsFileName the propsFileName to set
+   */
+  void setPropsFileName(String propsFileName) {
+    this.propsFileName = propsFileName;
+  }
 
-    /**
-     * @param simulatePrimaryISPIsDown the simulatePrimaryISPIsDown to set
-     */
-     void setSimulatePrimaryISPIsDown(boolean simulatePrimaryISPIsDown) {
-        this.simulatePrimaryISPIsDown = simulatePrimaryISPIsDown;
-    }
+  /**
+   * @return the props
+   */
+  Properties getProps() {
+    return props;
+  }
 
-    /**
-     * @param simulateBackupISPIsDown the simulateBackupISPIsDown to set
-     */
-     void setSimulateBackupISPIsDown(boolean simulateBackupISPIsDown) {
-        this.simulateBackupISPIsDown = simulateBackupISPIsDown;
-    }
+  /**
+   * @param props the props to set
+   */
+  void setProps(Properties props) {
+    this.props = props;
+  }
 
-    /**
-     * @param mockCheckISPisOK the mockCheckISPisOK to set
-     */
-     void setMockCheckISPisOK(boolean mockCheckISPisOK) {
-        this.mockCheckISPisOK = mockCheckISPisOK;
-    }
-    
+  /**
+   * @param propertiesSetTemporarely the propertiesSetTemporarely to set
+   */
+  void setPropertiesSetTemporarely(boolean propertiesSetTemporarely) {
+    this.propertiesSetTemporarely = propertiesSetTemporarely;
+  }
+
+  /**
+   * @return the hosts
+   */
+  List<String> getHosts() {
+    return hosts;
+  }
+
+  /**
+   * @param hosts the hosts to set
+   */
+  void setHosts(List<String> hosts) {
+    this.hosts = hosts;
+  }
+
+  /**
+   * @param simulatePrimaryISPIsDown the simulatePrimaryISPIsDown to set
+   */
+  void setSimulatePrimaryISPIsDown(boolean simulatePrimaryISPIsDown) {
+    this.simulatePrimaryISPIsDown = simulatePrimaryISPIsDown;
+  }
+
+  /**
+   * @param simulateBackupISPIsDown the simulateBackupISPIsDown to set
+   */
+  void setSimulateBackupISPIsDown(boolean simulateBackupISPIsDown) {
+    this.simulateBackupISPIsDown = simulateBackupISPIsDown;
+  }
+
+  /**
+   * @param mockCheckISPisOK the mockCheckISPisOK to set
+   */
+  void setMockCheckISPisOK(boolean mockCheckISPisOK) {
+    this.mockCheckISPisOK = mockCheckISPisOK;
+  }
+
 }
